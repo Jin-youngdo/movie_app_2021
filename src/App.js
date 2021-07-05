c8e399eab991c115d923ea6222b0987427569e06
Jin-youngdo <77922432+Jin-youngdo@users.noreply.github.com>
2021-07-05 23:08:46 +0900

    2021-07-05

--- src/App.js
+++ src/App.js
@@ -2,23 +2,67 @@
 // class component는 class형이기에 return 값이 존재하지 않지만,
 // React.Component를 extends(확장)함으로 screen 표시
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+  // axios를 통한 api값 불러오기
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    // this.setState({movies: movies})
+    //   line_7 = state{movies} : line_16 = movies
+  };
+
   componentDidMount() {
-    // delay function
+    /* // delay function
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 6000);
+    */
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
   }
 }
+
 /* 
 // react는 자동으로 class component의 render method를 실행한다.
 class App extends React.Component {
