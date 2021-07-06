9b52c7d8000e727740ed3efa461c1b5590f1b354
Jin-youngdo <77922432+Jin-youngdo@users.noreply.github.com>
2021-07-07 02:35:36 +0900

    detail-pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,37 @@
 import React from "react";
+import "./Detail.css";
 
-function Detail(props) {
-  console.log(props);
-  return <span>Hello!</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    const LS = location.state;
+    if (location.state) {
+      return (
+        <div className="detail__pages">
+          <img src={LS.poster} alt={LS.title} title={LS.title} />
+          <h3 className="detail__pages_title">{LS.title}</h3>
+          <h5 className="detail__pages_year">{LS.year}</h5>
+          <ul className="detail__pages_genres">
+            {LS.genres.map((genre, index) => (
+              <li key={index} className="detail__pages_genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="detail__pages_summary">{LS.summary}</p>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
